e92ea8e1d2e19e8e89b3f5b09771ed0f6fa70161
siyabonga mahlalela <siyabonga.mahlalela@imaginescholar.org>
2023-02-24 22:16:25 +0200

    Added some styling to the UI and improved the application's accuracysuccessfully made the ceasar cypher UI interactive

--- index.js
+++ index.js
@@ -1,43 +1,90 @@
-
-//Create alphabets array
-//Receive a string
-//
+ 
 const inputEl = document.getElementById("input-El")
 const Shift = document.getElementById("cypher-Shift")
 const outputEl = document.getElementById("output-El")
 const encryptBtn = document.getElementById("encrypt-Btn")
 
-let letter = 'A'
-let alphabets = []
-let message = ""
+let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-for (let i = 0; i < 26; i++) {
-    letter++;
-    alphabets[i] = letter
-}
-
-message += inputEl.textContent
 encryptBtn.addEventListener("click", function () {
-    encryptMessage(message)
+    inputEl.textContent = " "
+    outputEl.textContent = " ";
+    encryptMessage(inputEl.value)
 })
 
 function encryptMessage(_message) {
-
-    let mLength = message.length
-    let aLength = alphabets.length
-   // let encryption = ""
-
-    for (let i = 0; i < mLength; i++) {
-        for (let j = 0; j < aLength; j++) {
-            if (message[i] === alphabets[j]) {
-               outputEl.textContent += `${ (alphabets[j + 3]) % 26 }`
+	_message = _message.toUpperCase()
+	let j = 0, i = 0;
+	let mLength = _message.length
+	let aLength = alphabets.length
+	let encrypt = ""
+	let newLetter = ""
+	
+    for (i =0 ; i < mLength; i++) 
+    {
+        for (j =  0; j < aLength; j++) 
+        {
+			if(newLetter ==="Z"){
+				newLetter = alphabets[j*0 + (Shift.value % 26)] 
+			}
+			else if  (_message[i] === alphabets[j]) {
+				console.log(alphabets[j + Shift.value])
+				let newLetter = alphabets[ (j + (Shift.value % 26)).toString() ]
+				encrypt += `${newLetter}`
             }
+            else if (_message[i] === " ") {
+                encrypt += " "
+            }			       
         }
     }
-   //outputEl.textContent = encryption;
-    
+
+   return outputEl.textContent = `Encrypted Message: ${encrypt}`;   
+}
+/*			string sEncrypt = "";
+
+            for ( int i = 0; i < _sMessage.Length; i++)
+            {				
+				for (int j = 0; j < _cLetters.Length; j++) 
+				{
+					if (_sMessage[i] == _cLetters[j])
+					{
+						sEncrypt += (_cLetters[(j + _iShift) % 26]).ToString();//adding _iShift to the index of the _cLetters array, where _sMessage[i] == _cLetters[i] and saving it in the sEncrypt array.						 
+					}
+					
+				}
+			}			
+			return sEncrypt;
+
+function LetterIndex(_mLength, aLength, Shift){
+	_message = _message.toUpperCase()
+	let j = 0, i = 0;
+	let mLength = _message.length
+	let aLength = alphabets.length
+	let encrypt = ""
+	//let newLetter = ""
+	let l_Index = 0;
+
+	if(aLength == 1){
+		 l_Index = 0;
+	}
+
+	messageIndex(l_Index,mlength )
+	return l_Index = newMessage(aLength - 1)
 }
 
+function messageIndex(_j, _i){
+	m_Index = 0
+	if(i === 0){
+	m_Index = 0
+	}
+	newLetter()
+	return m_Index = messageIndex(_j, i -1)
+}
+
+function newLetter(){
+
+}
+*/
 
 
 
