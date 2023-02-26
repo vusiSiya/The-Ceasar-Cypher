3a07bdc214e4aed7a33e9dd1fb0ac436e0729530
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-02-26 18:23:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,51 +40,6 @@ function encryptMessage(_message) {
 
    return outputEl.textContent = `Encrypted Message: ${encrypt}`;   
 }
-/*			string sEncrypt = "";
-
-            for ( int i = 0; i < _sMessage.Length; i++)
-            {				
-				for (int j = 0; j < _cLetters.Length; j++) 
-				{
-					if (_sMessage[i] == _cLetters[j])
-					{
-						sEncrypt += (_cLetters[(j + _iShift) % 26]).ToString();//adding _iShift to the index of the _cLetters array, where _sMessage[i] == _cLetters[i] and saving it in the sEncrypt array.						 
-					}
-					
-				}
-			}			
-			return sEncrypt;
-
-function LetterIndex(_mLength, aLength, Shift){
-	_message = _message.toUpperCase()
-	let j = 0, i = 0;
-	let mLength = _message.length
-	let aLength = alphabets.length
-	let encrypt = ""
-	//let newLetter = ""
-	let l_Index = 0;
-
-	if(aLength == 1){
-		 l_Index = 0;
-	}
-
-	messageIndex(l_Index,mlength )
-	return l_Index = newMessage(aLength - 1)
-}
-
-function messageIndex(_j, _i){
-	m_Index = 0
-	if(i === 0){
-	m_Index = 0
-	}
-	newLetter()
-	return m_Index = messageIndex(_j, i -1)
-}
-
-function newLetter(){
-
-}
-*/
 
 
 
