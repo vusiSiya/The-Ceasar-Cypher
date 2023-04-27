10250afe5c8d554655f4781d8a17fb624ac89fe7
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-04-27 23:10:43 +0200

    Finally enabling the encryption to go beyond

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const inputEl = document.getElementById("input-El")
 const Shift = document.getElementById("cypher-Shift")
 const outputEl = document.getElementById("output-El")
 const encryptBtn = document.getElementById("encrypt-Btn")
+const ImageElement = document.getElementById("image-El")
 
 let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 encryptBtn.addEventListener("click", function() {
   inputEl.textContent = " "
   outputEl.textContent = " ";
+  ImageElement.innerHTML = `  <img src="boingBoing.png" id="cypher-Img" />`
   encryptMessage(inputEl.value)
+
 })
 
 function encryptMessage(_message) {
@@ -22,13 +25,10 @@ function encryptMessage(_message) {
 
   for (i = 0; i < mLength; i++) {
     for (j = 0; j < aLength; j++) {
-      if (newLetter === "Z") {
-        newLetter = alphabets[j * 0 + (Shift.value % 26)]
-      }
-      else if (_message[i] === alphabets[j]) {
-        console.log(alphabets[j + Shift.value])
-        let newLetter = alphabets[(j + (Shift.value % 26)).toString()]
-        encrypt += `${newLetter}`
+      if (_message[i] === alphabets[j]) {
+        let sum = j + Shift.value * 1
+        newLetter = alphabets[sum % 26]
+        encrypt = encrypt + `${newLetter}`
       }
       else if (_message[i] === " ") {
         encrypt += " "
@@ -38,13 +38,4 @@ function encryptMessage(_message) {
 
   return outputEl.textContent = `Encrypted Message: ${encrypt}`;
 }
-
-
-
-
-
-
-
-
-
 
